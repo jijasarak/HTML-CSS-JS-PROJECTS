2ebba960fc5e7b7fe44f359963c568b436913230
jijasarak <jijasarak@gmail.com>
2025-07-08 19:53:44 +0530

    update palindrome check logic

--- palindrome-checker/index.js
+++ palindrome-checker/index.js
@@ -1,11 +1,13 @@
 function checkPalindrome(){
   const input = document.querySelector("input");
   const text = input.value;
+  const inputText = text.replace(/[^a-zA-Z0-9]/g, "");
+
   let reverse = "";
-  for(let i = text.length-1; i >= 0; i--){
-    reverse += text[i];
+  for(let i = inputText.length-1; i >= 0; i--){
+    reverse += inputText[i];
   }
-  if(text.toLowerCase() === reverse.toLowerCase()){
+  if(inputText.toLowerCase() === reverse.toLowerCase()){
     document.getElementById("output").innerHTML = `Yes, <span>${"'"+text+"'"}</span> is a palindrome!`
   }else{
     document.getElementById("output").innerHTML = `No, <span>${"'"+text+"'"}</span> is not a palindrome!`
